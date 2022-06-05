3036193aed19d6ac4aadbc91c22979c89c1e7aa1
pramudyaw <101585179+pramudyaw@users.noreply.github.com>
2022-06-05 23:51:03 +0700

    Pramudya (Revisi routes)
    Pramudya (Revisi routes)

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'wisata';
 $route['TMII'] = 'wisata/TMII';
+$route['TMII'] = 'wisata/dufan';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
