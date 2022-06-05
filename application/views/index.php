2498ce26fa275ba09f4bc677b6a183018de433a5
banoesa607 <bmulia60.la@gmail.com>
2022-06-05 12:21:54 +0700

    Initial commit

---
+++ application/views/index.php
@@ -0,0 +1,441 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<title>Wisata Jakarta</title>
+
+  	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="">
+  	<meta name="description" content="">
+
+	<!-- stylesheets css -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+    <link rel="stylesheet" type="assets/text/css" href="assets/css/loaders.css"/>
+  	<link rel="stylesheet" href="assets/css/magnific-popup.css">
+	<link rel="stylesheet" href="assets/css/animate.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+  	<link rel="stylesheet" href="assets/css/nivo-lightbox.css">
+  	<link rel="stylesheet" href="assets/css/nivo_themes/default/default.css">
+  	<link rel="stylesheet" href="assets/css/hover-min.css">
+    <link rel="stylesheet" href="assets/css/contact-input-style.css">
+
+  	
+</head>
+<body>
+ <div class="loader loader-bg">
+        <div class="loader-inner ball-pulse-rise">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+
+<!------------Static navbar ------------>
+    <nav class="navbar navbar-default top-bar affix" data-spy="affix" data-offset-top="250" >
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#" >The Jakarta Tour</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          
+          <ul class="nav navbar-nav navbar-right navbar-link">
+            <li><a href="#banner">Home</a></li>
+              <li><a href="#todo">Wisata</a></li>
+              <li><a href="#video-sec">Video</a></li>
+              <li><a href="#location">Lokasi</a></li>
+              <li><a href="#hotels">Hotel</a></li>
+              <li><a href="#contact-sec">Kontak Kami</a></li>
+              <li><a href="#tentang">Tentang Kami</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+
+    
+<!------------ Home Banner ------------>
+<section id="banner" class="parallax">
+  <div class="gradient-overlay"> <img src="" width="2000"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-offset-2 col-md-8 col-sm-12">
+              <h1 class="wow fadeInUp" data-wow-delay="1s">Liburan Impian</h1>
+              <p class="wow fadeInUp" data-wow-delay="1s">Dunia adalah sebuah buku, dan mereka yang tidak melakukan perjalanan hanya membaca satu halaman.<br>
+                  <small>Untuk sekali Anda telah mencicipi penerbangan Anda akan berjalan di bumi dengan mata melihat ke atas, karena di sana Anda telah dan di sana Anda akan rindu untuk kembali.</small>
+                  </p>
+              <a href="#todo" class="wow fadeInUp btn btn-transparent-white btn-capsul btn-lg smoothScroll" data-wow-delay="1.3s">Selengkapnya</a>
+          </div>
+      </div>
+    </div>
+</section>
+
+
+<!------------ todo Section ------------>
+<section id="todo" class="parallax">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-md-offset-2 col-md-8 col-sm-offset-1 col-sm-10 text-center">
+          <div class="wow fadeInUp section-title" data-wow-delay="0.6s">
+            <h2>Wisata Rekomendasi Kami<small>Siapkan Perjalanan Anda</small></h2>
+          </div>
+      </div>
+
+      <div class="clearfix"></div>
+
+      <div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+        <div class="feature-thumb">
+          <div class="icon">
+             <span><img class="img-responsive" src="assets/img/icon1.jpg"/></span>
+          </div>
+          <h3>MoJa Museum</h3>
+          <p>Komplek Gelora Bung Karno, Main Stadium, Jl. Gerbang Pemuda No.1, RT.1/RW.3, Gelora, Kecamatan Tanah Abang, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10270</p>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.6s">
+        <div class="feature-thumb">
+          <div class="icon">
+            <span><img class="img-responsive" src="assets/img/icon2.jpg" /></span>
+          </div>
+          <h3>Monumen Nasional</h3>
+          <p>Gambir, Kecamatan Gambir, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta</p>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.9s">
+        <div class="feature-thumb">
+          <div class="icon">
+            <span><img class="img-responsive" src="assets/img/icon3.png" /></span>
+          </div>
+           <h3>Kota Tua</h3>
+           <p>Mangga Besar, Kec. Taman Sari, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta</p>
+        </div>
+      </div>
+    </div>  
+        <div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+        <div class="feature-thumb">
+          <div class="icon">
+             <span><img class="img-responsive" src="assets/img/icon5.jpg" /></span>
+          </div>
+          <h3>Dunia Fantasi</h3>
+          <p>Jl. Lodan Timur No.7, RW.10, Ancol, Kec. Pademangan, Kota Jkt Utara, Daerah Khusus Ibukota Jakarta 14430</p>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.6s">
+        <div class="feature-thumb">
+          <div class="icon">
+            <span><img class="img-responsive" src="assets/img/icon4.png" /></span>
+          </div>
+          <h3>Taman Impian Jaya Ancol</h3>
+          <p>Jl. Lodan Timur No.7, RW.10, Ancol, Kec. Pademangan, Kota Jkt Utara, Daerah Khusus Ibukota Jakarta 14430</p>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.9s">
+        <div class="feature-thumb">
+          <div class="icon">
+            <span><img class="img-responsive" src="assets/img/icon6.jpg" /></span>
+          </div>
+           <h3>Taman Mini Indonesia Indah</h3>
+           <p>Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta</p>
+        </div>
+    </div>
+  </div>
+</section>
+
+
+<!------------ Video section ------------>
+<section id="video-sec" class="parallax">
+  <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+
+          <div class="col-md-offset-2 col-md-8 col-sm-12">
+              <h2 class="wow fadeInUp" data-wow-delay="0.5s">Lihat Video Kami<small>Semua perjalanan memiliki tujuan rahasia </small></h2>
+              <a class="popup-youtube" href="https://www.youtube.com/watch?v=KxiczcSsTDw" ><i class="fa fa-play"></i></a>
+              <small><em>Video by: Youtube | Expedia</em></small>
+          </div>
+      </div>
+    </div>
+</section>
+
+<!------------ Menu section ------------>
+<section id="location" class="parallax">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-md-offset-2 col-md-8 col-sm-offset-1 col-sm-10">
+         <div class="wow fadeInUp section-title" data-wow-delay="1s">
+            <h2>Lokasi Wisata Dengan Rating Tinggi <small>Setiap hari itu adalah perjalanan</small></h2>
+        </div>
+      </div>
+
+      <div class="col-md-6 col-sm-12">
+        <div class="media wow fadeInUp" data-wow-delay="0.5s">
+          <div class="media-middle">
+            <img src="assets/img/icon6.jpg" class="img-responsive" alt="Kota Tua">
+          </div>
+          <div class="media-body">
+            <h3 class="media-heading">Taman Mini Indonesia Indah</h3>
+            <p></p>
+              <a href="#" class="btn btn-capsul btn-transparent-prime">Explore Now</a>
+          </div>
+        </div>
+
+        <div class="media wow fadeInUp" data-wow-delay="0.9s">
+          <div class="media-middle ">
+            <img src="assets/img/icon5.jpg" class="img-responsive" alt="">
+          </div>
+          <div class="media-body">
+            <h3 class="media-heading">Dunia Fantasi</h3>
+            <p>Eiffel Tower and Gothic Notre-Dame cathedral, the city is known for its cafe culture and designer boutiques along the Rue du Faubourg Saint-Honoré</p>
+             <a href="#" class="btn btn-capsul btn-transparent-prime">Explore Now</a>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col-md-6 col-sm-12">
+        <div class="media wow fadeInUp" data-wow-delay="0.5s">
+          <div class="media-middle">
+            <img src="assets/img/icon3.png" class="img-responsive" alt="">
+           </div>
+          <div class="media-body">
+            <h3 class="media-heading">Kota Tua</h3>
+            <p>At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers</p>
+              <a href="#" class="btn btn-capsul btn-transparent-prime">Explore Now</a>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</section>
+
+
+<!------------ Gallery section ------------>
+<section id="hotels" class="parallax">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-md-12">
+
+       <div class="col-md-offset-2 col-md-8 col-sm-offset-1 col-sm-10 text-center">
+         <div class="wow fadeInUp section-title" data-wow-delay="1s">
+            <h2>Best Hotels out there<small>The great advantage of a hotel is that it is a refuge from home life.</small></h2>
+        </div>
+      </div>
+        
+          <!-- iso section -->
+          <div class="iso-section wow fadeInUp text-center" data-wow-delay="0.5">
+
+            		<ul class="filter-wrapper clearfix">
+                        <li><a href="#" data-filter="*" class="selected opc-main-bg">All</a></li>
+                        <li><a href="#" class="opc-main-bg" data-filter=".london">London</a></li>
+                        <li><a href="#" class="opc-main-bg" data-filter=".paris">Paris</a></li>
+                        <li><a href="#" class="opc-main-bg" data-filter=".ny">New York</a></li>
+                        <li><a href="#" class="opc-main-bg" data-filter=".hn">Hong Kong</a></li>
+                    </ul>
+
+                    <!-- iso box section -->
+                    <div class="iso-box-section wow fadeInUp" data-wow-delay="0.9s">
+                      <div class="iso-box-wrapper col4-iso-box">
+
+                        <div class="iso-box london paris ny col-md-4 col-sm-6">
+                          <div class="gallery-thumb">
+                            <a href="images/hotel-01.jpg" data-lightbox-gallery="food-gallery">
+                              <img src="images/hotel-01.jpg" class="fluid-img" alt="Gallery">
+                                <div class="gallery-overlay">
+                                  <div class="gallery-item">
+                                    <i class="fa fa-search"></i>
+                                  </div>
+                                </div>
+                            </a>
+                          </div>
+                          <h3>Hotel Name</h3>
+                        </div>
+
+                        <div class="iso-box london ny hn col-md-4 col-sm-6">
+                          <div class="gallery-thumb">
+                            <a href="images/hotel-02.jpg" data-lightbox-gallery="food-gallery">
+                              <img src="images/hotel-02.jpg" class="fluid-img" alt="Gallery">
+                                <div class="gallery-overlay">
+                                  <div class="gallery-item">
+                                    <i class="fa fa-search"></i>
+                                  </div>
+                                </div>
+                            </a>
+                          </div>
+                          <h3>Hotel Name Two</h3>
+                        </div>
+
+                        <div class="iso-box hn col-md-4 col-sm-6">
+                          <div class="gallery-thumb">
+                            <a href="images/hotel-03.jpg" data-lightbox-gallery="food-gallery">
+                              <img src="images/hotel-03.jpg" class="fluid-img" alt="Gallery">
+                                <div class="gallery-overlay">
+                                  <div class="gallery-item">
+                                    <i class="fa fa-search"></i>
+                                  </div>
+                                </div>
+                            </a>
+                          </div>
+                          <h3>Hotel Name Three</h3>
+                        </div>
+
+                        <div class="iso-box london col-md-4 col-sm-6">
+                          <div class="gallery-thumb">
+                            <a href="images/hotel-04.jpg" data-lightbox-gallery="food-gallery">
+                              <img src="images/hotel-04.jpg" class="fluid-img" alt="Gallery">
+                                <div class="gallery-overlay">
+                                  <div class="gallery-item">
+                                    <i class="fa fa-search"></i>
+                                  </div>
+                                </div>
+                            </a>
+                          </div>
+                          <h3>Hotel Name Four</h3>
+                        </div>
+
+                        <div class="iso-box ny col-md-4 col-sm-6">
+                          <div class="gallery-thumb">
+                            <a href="images/hotel-05.jpg" data-lightbox-gallery="food-gallery">
+                              <img src="images/hotel-05.jpg" class="fluid-img" alt="Gallery">
+                                <div class="gallery-overlay">
+                                  <div class="gallery-item">
+                                    <i class="fa fa-search"></i>
+                                  </div>
+                                </div>
+                            </a>
+                          </div>
+                          <h3>Hotel Name Five</h3>
+                        </div>
+
+                        <div class="iso-box paris lunch col-md-4 col-sm-6">
+                          <div class="gallery-thumb">
+                            <a href="images/hotel-06.jpg" data-lightbox-gallery="food-gallery">
+                              <img src="images/hotel-06.jpg" class="fluid-img" alt="Gallery">
+                                <div class="gallery-overlay">
+                                  <div class="gallery-item">
+                                    <i class="fa fa-search"></i>
+                                  </div>
+                                </div>
+                            </a>
+                          </div>
+                          <h3>Hotel Name Six</h3>
+                        </div>
+
+                       </div>
+                    </div>
+
+          </div>
+
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<!----Tentang Kami---->
+<section id="tentang" class="parallax">
+  <div class="gradient"> </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-offset-2 col-md-8 col-sm-12">
+              <h1 class="wow fadeInUp" data-wow-delay="1s">The Jakarta Tour</h1>
+              <p class="wow fadeInUp" data-wow-delay="1s">Website Yang Memberikan Informasi Tentang Wisata Khususnya Untuk Wilayah Jakarta, Sesuai dengan Trendi dan Saran Para Travelers
+                  </p>
+          </div>
+      </div>
+    </div>
+</section>
+
+<!------------ Contact section ------------>
+<section id="contact-sec" class=" content">
+  <div class="overlay"></div>
+	<div class="container form-block">
+                <div class="row">
+                <div class="col-lg-12 text-center"><h2>Kontak Kami<small>Berikan Saran Kalian Agar Kami Bisa Menjadi Lebih Baik</small></h2></div>
+				<div class="col-lg-4 col-md-4 col-sm-12">
+                <span class="input input-hoshi">
+					<input class="input_field input_field-hoshi" type="text" id="input-4" />
+					<label class="input_label input_label-hoshi input_label-hoshi-color-1" for="input-4">
+						<span class="input_label-content input_label-content-hoshi">Name</span>
+					</label>
+				</span>
+                </div>
+				<div class="col-lg-4 col-md-4 col-sm-12">
+                <span class="input input-hoshi">
+					<input class="input_field input_field-hoshi" type="text" id="input-4" />
+					<label class="input_label input_label-hoshi input_label-hoshi-color-1" for="input-4">
+						<span class="input_label-content input_label-content-hoshi">Phone</span>
+					</label>
+				</span>
+                </div>
+				<div class="col-lg-4 col-md-4 col-sm-12">
+                <span class="input input-hoshi">
+					<input class="input_field input_field-hoshi" type="text" id="input-4" />
+					<label class="input_label input_label-hoshi input_label-hoshi-color-1" for="input-4">
+						<span class="input_label-content input_label-content-hoshi">Email</span>
+					</label>
+				</span>
+                </div>
+                
+                <div class="col-lg-12 col-md-12 col-sm-12 text-center">
+                <span class="t-area input-hoshi">
+                <textarea class="input_field input_field-hoshi" rows="1"></textarea>
+						<label class="input_label input_label-hoshi input_label-hoshi-color-1" for="input-4">
+						<span class="input_label-content input_label-content-hoshi">Saran</span>
+					</label>
+				</span>
+                <a href="#" class="btn btn-capsul btn-transparent-white">Submit</a>
+                </div>
+                
+                </div>
+                </div>
+</section>
+
+<!------------ Footer section ------------>
+<footer>
+    
+        <div class="container">
+            <div class="row">
+               Shared by <i class="fa fa-love"></i><a href="https://bootstrapthemes.co">BootstrapThemes</a>
+
+            </div>
+        
+        </div>
+    
+    </footer>
+
+
+
+<script src="assets/js/jquery.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/jquery.backstretch.min.js"></script>
+<script src="assets/js/isotope.js"></script>
+<script src="assets/js/imagesloaded.min.js"></script>
+<script src="assets/js/nivo-lightbox.min.js"></script>
+<script src="assets/js/jquery.parallax.js"></script>
+<script src="assets/js/smoothscroll.js"></script>
+<script src="assets/js/wow.min.js"></script>
+<script src="assets/js/core.js"></script>
+
+
+</body>
+</html>
---
+++ application/views/tentang.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<title>The Jakarta Tour</title>
+
+  	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="">
+  	<meta name="description" content="">
+
+	<!-- stylesheets css -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" type="text/css" href="css/loaders.css"/>
+  	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="css/animate.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+  	<link rel="stylesheet" href="css/nivo-lightbox.css">
+  	<link rel="stylesheet" href="css/nivo_themes/default/default.css">
+  	<link rel="stylesheet" href="css/hover-min.css">
+    <link rel="stylesheet" href="css/contact-input-style.css">
+
+  	
+</head>
+<body>
+ <div class="loader loader-bg">
+        <div class="loader-inner ball-pulse-rise">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+
+<!------------Static navbar ------------>
+    <nav class="navbar navbar-default top-bar affix" data-spy="affix" data-offset-top="250" >
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#" >The Jakarta Tour</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          
+          <ul class="nav navbar-nav navbar-right navbar-link">
+            <li><a href="#banner">Home</a></li>
+              <li><a href="#todo">Wisata</a></li>
+              <li><a href="#video-sec">Video</a></li>
+              <li><a href="#location">Lokasi</a></li>
+              <li><a href="#hotels">Hotel</a></li>
+              <li><a href="#contact-sec">Kontak Kami</a></li>
+              <li><a href="#tentang">Tentang Kami</a></li> 
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+
+<!----Tentang Kami---->
+<section id="tentang" class="parallax">
+    <div class="gradient"> </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-offset-2 col-md-8 col-sm-12">
+                <h2 class="wow fadeInUp" data-wow-delay="1s">Website Kami</h2>
+                <p class="wow fadeInUp" data-wow-delay="1s">Memberikan Informasi Tentang Wisata Khususnya Untuk Wilayah Jakarta, Sesuai dengan Trendi dan Saran Para Travelers
+                    </p>
+            </div>
+        </div>
+      </div>
+  </section>
