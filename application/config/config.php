403f422171b3187c0e94db7ecc1e71cb9a439f57
banoesa607 <bmulia60.la@gmail.com>
2022-06-05 13:59:41 +0700

    ubah base URL

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost/Pariwisata_Jakarta';
+$config['base_url'] = 'https://banoesa607.github.io/';
 
 /*
 |--------------------------------------------------------------------------
