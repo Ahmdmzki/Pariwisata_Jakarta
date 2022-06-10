7a9e65f535c75f5684bf2b341b39d33105ffbbf8
kahfiprayoga <kahfiprayoga8@gmail.com>
2022-06-10 15:06:27 +0700

    BAGUSIN LOGIN

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,22 +1,54 @@
+
+<style type="text/css">
+    table {
+        font-family: Verdana, Arial, Helvetica, sans-serif;
+        font-size: 11px;
+        background-color: "white";
+    }
+    input {
+        font-family: Verdana, Arial, Helvetica, sans-serif;
+        font-size: 15px;
+        height: 30px;
+        border-radius: 1rem;
+        padding: 1rem;
+
+    }
+    div {
+        border-radius: 1rem;
+    }
+    label {
+        color: #e1eec3;
+    }
+    button {
+        border-radius: 1rem;
+        padding: 3%;
+    }
+</style>
+    <body bgcolor="white"><br />
+        <div align="center">
 <section id="todo" class="parallax">
-    <div style="color: white; padding: 1rem; height: 100%;">
+    <div style="box-shadow: -10px -10px 10px 21px rgba(0,0,0,0.35); background-color: radial-gradient(#f05053 50%, #e1eec3); color: black; padding: 6rem; width: 30% ;height: 30% ;">
         <form action="login" method="POST">
-            <h3>Login</h3>
+            <h3 style="font-family:times new roman; color: white;">LOGIN</h3>
             <Label>Username</Label>
-            <input name="username" style="color: black;" type="text">
+            <br>
+            <input name="username" style="color: black;" type="text" placeholder="Username">
             <?= form_error('username'); ?>
             <br>
-            <label>Password</label>
-            <input name="password" style="color: black;" type="password">
+            <br>
+            <label>Password </label>
+            <br>
+            <input name="password" style="color: black;" type="password" placeholder="Password">
             <?= form_error('password'); ?>
             <br>
-            <button type="submit" style="color: black;">Submit</button>
+            <br>
+            <button type="submit" style="font-family: 'Times New Roman', Times, serif; color: #6495ED;"><b>LOGIN</b></button>
+            <button type="submit" style="font-family: 'Times New Roman', Times, serif; color: blue;"><b><a href="register">REGISTER</a></b></button>
             <?php
             $message = $this->session->flashdata('message');
             if (isset($message)) {
                 echo $message;
             }
-            // echo "<p style='color: red;'> " . $message . " </p>";
             ?>
         </form>
 
