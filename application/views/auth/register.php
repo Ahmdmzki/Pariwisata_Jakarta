739365965108f665bbd399271d01c6c124e1f295
Ahmdmzki <101920522+Ahmdmzki@users.noreply.github.com>
2022-06-10 23:41:42 +0700

    Bagusin Register

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -1,21 +1,51 @@
+<style type="text/css">
+    table {
+        font-family: Verdana, Arial, Helvetica, sans-serif;
+        font-size: 11px;
+        background-color: "white";
+    }
+    input {
+        font-family: Verdana, Arial, Helvetica, sans-serif;
+        font-size: 15px;
+        height: 30px;
+        border-radius: 1rem;
+        padding: 1rem;
+
+    }
+    div {
+        border-radius: 1rem;
+    }
+    label {
+        color: #e1eec3;
+    }
+    button {
+        border-radius: 1rem;
+        padding: 3%;
+    }
+</style>
+
 <section id="todo" class="parallax">
-    <div style="color: white; padding: 1rem; height: 100%;">
+    <div style="background-color: radial-gradient(#f05053 50%, #e1eec3); color: black; padding: 6rem; width: 30% ;height: 30% ;">
         <form action="register" method="post">
-            <h3>Register</h3>
+            <h3 style="color: white">REGISTER</h3>
             <Label>Username</Label>
             <br>
-            <input name="username" style="color: black;" type="text">
+            <input name="username" style="color: black;" type="text" placeholder="Username">
             <?= form_error('username'); ?>
             <br>
             <label>Password</label>
-            <input name="password" style="color: black;" type="password">
+            <br>
+            <input name="password" style="color: black;" type="password" placeholder="Password">
             <?= form_error('password'); ?>
             <br>
             <label>Repeat Password</label>
-            <input name="repeat_password" style="color: black;" type="password">
+            <br>
+            <input name="repeat_password" style="color: black;" type="password" placeholder="Repeat Password">
             <?= form_error('repeat_password'); ?>
             <br>
-            <button type="submit" style="color: black;">Submit</button>
+            <br>
+            <button type="submit" style="font-family: 'Times New Roman', Times, serif; color: #6495ED;"><b>SUBMIT</b></button>
+            <button type="submit" style="font-family: 'Times New Roman', Times, serif; color: #6495ED;"><b><a href="login">LOGIN</a></b></button>
         </form>
     </div>
 
